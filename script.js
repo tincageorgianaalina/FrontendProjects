3b9997a04ba843bf6fc2fb667483093e3ea27950
tincageorgianaalina <tincageorgianaalina@gmail.com>
2023-06-01 08:54:41 +0100

    add recipe_how_to_make_tea

---
+++ script.js
@@ -0,0 +1,12 @@
+function ingredientsHover() {
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "300%";
+  }
+  function ingredientsNormal() {
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "100%";
+  }
+  function preparationHover() {
+    document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "300%";
+  }
+  function preparationNormal() {
+    document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "100%";
+  } 
